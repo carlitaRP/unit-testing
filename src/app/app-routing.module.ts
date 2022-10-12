81194efc311367a80bff61d87f3a66412f331d86
carlitaRP <carlapro2002@gmail.com>
2022-10-12 09:33:22 -0500

    cambios #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
-
-@NgModule({ 
+   
+@NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
